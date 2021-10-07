5792c4cef35f93fb72c6da2847e7239c3d58ce3b
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-08 00:50:10 +0200

    Add PackageActivated.php and PackageDeactivated.php events

---
+++ src/Events/PackageActivated.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Latus\ComposerPlugins\Events;
+
+class PackageActivated extends PackageEvent
+{
+}
---
+++ src/Events/PackageDeactivated.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Latus\ComposerPlugins\Events;
+
+class PackageDeactivated extends PackageEvent
+{
+}
